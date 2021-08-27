3d1bdec6401be674f0a921570d015acfc65dc940
Tim Jensen <tim.l.jensen@gmail.com>
2021-08-27 08:12:06 -0500

    Cleaning up whitespace because tabs are gross

--- scripts/4_world/holograms.c
+++ scripts/4_world/holograms.c
@@ -1,27 +1,27 @@
 modded class Hologram
-{	
-	override string ProjectionBasedOnParent()
-	{
-		MoreDoorKitBase item_in_hands = MoreDoorKitBase.Cast( m_Player.GetHumanInventory().GetEntityInHands() );
+{
+    override string ProjectionBasedOnParent()
+    {
+        MoreDoorKitBase item_in_hands = MoreDoorKitBase.Cast( m_Player.GetHumanInventory().GetEntityInHands() );
 
-		if ( item_in_hands )
-		{
-			return item_in_hands.j_Door();
-		}
-		
-		return super.ProjectionBasedOnParent();
-	}
-	
-	override void EvaluateCollision(ItemBase action_item = null)
-	{	
-		ItemBase item_in_hands = ItemBase.Cast( m_Player.GetHumanInventory().GetEntityInHands() );
+        if ( item_in_hands )
+        {
+            return item_in_hands.j_Door();
+        }
 
-		if ( item_in_hands && item_in_hands.IsInherited( MoreDoorKitBase ))
-		{
-			SetIsColliding(false);
-			return;
-		}
-			
-		super.EvaluateCollision();
-	}
+        return super.ProjectionBasedOnParent();
+    }
+
+    override void EvaluateCollision(ItemBase action_item = null)
+    {
+        ItemBase item_in_hands = ItemBase.Cast( m_Player.GetHumanInventory().GetEntityInHands() );
+
+        if ( item_in_hands && item_in_hands.IsInherited( MoreDoorKitBase ))
+        {
+            SetIsColliding(false);
+            return;
+        }
+
+        super.EvaluateCollision();
+    }
 }
