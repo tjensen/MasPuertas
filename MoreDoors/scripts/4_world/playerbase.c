c7563e6e8e9c7be5243be5031abd7de50c7bef13
Tim Jensen <tim.l.jensen@gmail.com>
2022-01-17 13:36:10 -0600

    Prevent climbing over small doors

--- MoreDoors/scripts/4_world/playerbase.c
+++ MoreDoors/scripts/4_world/playerbase.c
@@ -1,18 +1,30 @@
 modded class PlayerBase
 {
-	override void SetActions(out TInputActionMap InputActionMap)
+    override void SetActions(out TInputActionMap InputActionMap)
     {
         super.SetActions(InputActionMap);
 
-		AddAction(ActionDestroyMoreDoor, InputActionMap);
-	}
+        AddAction(ActionDestroyMoreDoor, InputActionMap);
+    }
+
+    override bool CanClimb(int climbType, SHumanCommandClimbResult climbRes)
+    {
+        if (climbType == 2 && climbRes.m_GrabPointParent.IsInherited(MoreDoorBase))
+        {
+            return false;
+        }
+
+        return super.CanClimb(climbType, climbRes);
+    }
 };
 
 modded class ModItemRegisterCallbacks
 {
     override void RegisterHeavy(DayZPlayerType pType, DayzPlayerItemBehaviorCfg pBehavior)
     {
-        super.RegisterHeavy( pType, pBehavior );     
+        super.RegisterHeavy(pType, pBehavior);
         pType.AddItemInHandsProfileIK("MoreDoorKitBase", "dz/anims/workspaces/player/player_main/player_main_heavy.asi", pBehavior, "dz/anims/anm/player/ik/heavy/wooden_crate.anm");
     }
-};  
+};
+
+// vim:ft=enforce
