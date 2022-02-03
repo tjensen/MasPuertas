43a67b6b185542faaa6c07335163bcee076f4cec
Tim Jensen <tim.l.jensen@gmail.com>
2022-02-02 18:36:21 -0600

    Tabs are gross

--- MoreDoors/scripts/4_world/customclasses/moredoorbase.c
+++ MoreDoors/scripts/4_world/customclasses/moredoorbase.c
@@ -1,451 +1,451 @@
 class MoreDoorBase extends Fence
 {
-	//protected bool 			m_DismantleDisabled;
+    //protected bool            m_DismantleDisabled;
 
-	// void MoreDoorBase()
-	// {
-	// 	RegisterNetSyncVariableBool( "m_DismantleDisabled" );
-	// }
+    // void MoreDoorBase()
+    // {
+    //  RegisterNetSyncVariableBool( "m_DismantleDisabled" );
+    // }
 
-	// bool DismantleDisabled()
-	// {
-	// 	return m_DismantleDisabled;
-	// }
+    // bool DismantleDisabled()
+    // {
+    //  return m_DismantleDisabled;
+    // }
 
-	// void SetDismantleAbility( bool state )
-	// {
-	// 	m_DismantleDisabled = state;
-	// 	SetSynchDirty();
-	// }
+    // void SetDismantleAbility( bool state )
+    // {
+    //  m_DismantleDisabled = state;
+    //  SetSynchDirty();
+    // }
 
-	bool HasGate()
-	{
-		return true;
-	}
+    bool HasGate()
+    {
+        return true;
+    }
 
-	override bool HasHinges()
-	{
-		return true;
-	}
+    override bool HasHinges()
+    {
+        return true;
+    }
 
-	bool IsMoreDoor()
-	{
-		return true;
-	}
+    bool IsMoreDoor()
+    {
+        return true;
+    }
 
-	override void AfterStoreLoad()
-	{	
-		SetGateState( true );
-		SetBaseState( true );
-	}
+    override void AfterStoreLoad()
+    {
+        SetGateState( true );
+        SetBaseState( true );
+    }
 
-	string j_moreDoorKit()
-	{
-		return "MoreDoorSmallKit";
-	}
+    string j_moreDoorKit()
+    {
+        return "MoreDoorSmallKit";
+    }
 
-	bool AllowDismantle()
-	{
-		return true;
-	}
+    bool AllowDismantle()
+    {
+        return true;
+    }
 
-	bool AllowDestruction()
-	{
-		return true;
-	}
+    bool AllowDestruction()
+    {
+        return true;
+    }
 
-	bool IsMetalDoor()
-	{
-		return false;
-	}
+    bool IsMetalDoor()
+    {
+        return false;
+    }
 
-	override bool CanCloseFence()
-	{
-		if ( IsOpened() )
-		{
-			return true;
-		}
-		
-		return false;
-	}
+    override bool CanCloseFence()
+    {
+        if ( IsOpened() )
+        {
+            return true;
+        }
+
+        return false;
+    }
 
     override void OpenFence()
-	{
-		//server or single player
-		if ( GetGame().IsServer() )
-		{
-			SetAnimationPhase( "Wall_Gate_Rotate", 					2 );
-			SetOpenedState( true );
-			
-			//regenerate navmesh
-			GetGame().GetCallQueue( CALL_CATEGORY_GAMEPLAY ).CallLater( UpdateNavmesh, GATE_ROTATION_TIME_APPROX, false );
-			
-			//synchronize
-			SynchronizeBaseState();
-		}
-		
-		//client or single player
-		if ( !GetGame().IsMultiplayer() || GetGame().IsClient() )
-		{
-			//play sound
-			SoundGateOpenStart();
-		}
-	}
+    {
+        //server or single player
+        if ( GetGame().IsServer() )
+        {
+            SetAnimationPhase( "Wall_Gate_Rotate",                  2 );
+            SetOpenedState( true );
 
-	override void SetPartsAfterStoreLoad()
-	{
-		//update server data
-		SetPartsFromSyncData();
-		
-		//set base state
-		SetBaseState( true ) ;
-			
-		//synchronize after load
-		SynchronizeBaseState();
-	}
+            //regenerate navmesh
+            GetGame().GetCallQueue( CALL_CATEGORY_GAMEPLAY ).CallLater( UpdateNavmesh, GATE_ROTATION_TIME_APPROX, false );
 
-	override bool NameOverride(out string output)
+            //synchronize
+            SynchronizeBaseState();
+        }
+
+        //client or single player
+        if ( !GetGame().IsMultiplayer() || GetGame().IsClient() )
+        {
+            //play sound
+            SoundGateOpenStart();
+        }
+    }
+
+    override void SetPartsAfterStoreLoad()
+    {
+        //update server data
+        SetPartsFromSyncData();
+
+        //set base state
+        SetBaseState( true ) ;
+
+        //synchronize after load
+        SynchronizeBaseState();
+    }
+
+    override bool NameOverride(out string output)
     {
         return false;
     }
 
     override bool CanDisplayAttachmentSlot( string slot_name )
-	{
-		return true;
-	}
+    {
+        return true;
+    }
 
-	override bool CanDisplayAttachmentCategory( string category_name )
-	{
-		return true;
-	}
+    override bool CanDisplayAttachmentCategory( string category_name )
+    {
+        return true;
+    }
 
-	override bool CanReceiveAttachment( EntityAI attachment, int slotId )
-	{
-		return true;
-	}
+    override bool CanReceiveAttachment( EntityAI attachment, int slotId )
+    {
+        return true;
+    }
 
-	override void SetActions()
-	{
-		super.SetActions();
-		
-		AddAction(ActionDialCombinationLockOnMoreDoor);
-		AddAction(ActionNextCombinationLockDialOnMoreDoor);
-		AddAction(ActionOpenMoreDoor);
-		AddAction(ActionCloseMoreDoor);
-		RemoveAction(ActionFoldBaseBuildingObject);
-		RemoveAction(ActionOpenFence);
-		RemoveAction(ActionCloseFence);
-	}
+    override void SetActions()
+    {
+        super.SetActions();
 
-	// override void OnStoreSave( ParamsWriteContext ctx )
-	// {   
-	// 	super.OnStoreSave( ctx );
-		
-	// 	//write
-	// 	ctx.Write( m_DismantleDisabled );
-	// }
+        AddAction(ActionDialCombinationLockOnMoreDoor);
+        AddAction(ActionNextCombinationLockDialOnMoreDoor);
+        AddAction(ActionOpenMoreDoor);
+        AddAction(ActionCloseMoreDoor);
+        RemoveAction(ActionFoldBaseBuildingObject);
+        RemoveAction(ActionOpenFence);
+        RemoveAction(ActionCloseFence);
+    }
 
-	// override bool OnStoreLoad( ParamsReadContext ctx, int version )
-	// {
-	// 	if ( !super.OnStoreLoad( ctx, version ) )
-	// 		return false;
-		
-	// 	//is opened
-	// 	if ( !ctx.Read( m_IsOpened ) )
-	// 	{
-	// 		m_IsOpened = false;
-	// 		return false;
-	// 	}
+    // override void OnStoreSave( ParamsWriteContext ctx )
+    // {
+    //  super.OnStoreSave( ctx );
 
-	// 	if ( !ctx.Read( m_DismantleDisabled ) )
-	// 	{
-	// 		m_DismantleDisabled = false;
-	// 		return false;
-	// 	}
-		
-	// 	return true;
-	// }
+    //  //write
+    //  ctx.Write( m_DismantleDisabled );
+    // }
 
-	// override void EEItemDetached ( EntityAI item, string slot_name )
-	// {
-	// 	super.EEItemDetached ( item, slot_name );
+    // override bool OnStoreLoad( ParamsReadContext ctx, int version )
+    // {
+    //  if ( !super.OnStoreLoad( ctx, version ) )
+    //      return false;
 
-	// 	if(DismantleDisabled())
-	// 	{
-	// 		SetDismantleAbility(false);
-	// 	}
-	// }
+    //  //is opened
+    //  if ( !ctx.Read( m_IsOpened ) )
+    //  {
+    //      m_IsOpened = false;
+    //      return false;
+    //  }
+
+    //  if ( !ctx.Read( m_DismantleDisabled ) )
+    //  {
+    //      m_DismantleDisabled = false;
+    //      return false;
+    //  }
+
+    //  return true;
+    // }
+
+    // override void EEItemDetached ( EntityAI item, string slot_name )
+    // {
+    //  super.EEItemDetached ( item, slot_name );
+
+    //  if(DismantleDisabled())
+    //  {
+    //      SetDismantleAbility(false);
+    //  }
+    // }
 
 };
 
 class MoreDoorVault extends MoreDoorBase
 {
-	const string SOUND_SAFE_OPEN_START		= "MoreDoor_doorMetalSlideLargeOpen_SoundSet";
-	const string SOUND_SAFE_CLOSE_START		= "MoreDoor_doorMetalSlideLargeClose_SoundSet";
-	const string SOUND_SAFE_CLOSE_END 		= "MoreDoor_doorMetalGateClose_SoundShader";
+    const string SOUND_SAFE_OPEN_START      = "MoreDoor_doorMetalSlideLargeOpen_SoundSet";
+    const string SOUND_SAFE_CLOSE_START     = "MoreDoor_doorMetalSlideLargeClose_SoundSet";
+    const string SOUND_SAFE_CLOSE_END       = "MoreDoor_doorMetalGateClose_SoundShader";
 
-	override string j_moreDoorKit()
-	{
-		return "MoreDoorVaultKit";
-	}
+    override string j_moreDoorKit()
+    {
+        return "MoreDoorVaultKit";
+    }
 
-	override bool AllowDestruction()
-	{
-		return false;
-	}
+    override bool AllowDestruction()
+    {
+        return false;
+    }
 
-	override protected void SoundGateOpenStart()
-	{
-		PlaySoundSet( m_SoundGate_Start, SOUND_SAFE_OPEN_START, 0.1, 0.1 );
-	}
+    override protected void SoundGateOpenStart()
+    {
+        PlaySoundSet( m_SoundGate_Start, SOUND_SAFE_OPEN_START, 0.1, 0.1 );
+    }
 
-	override protected void SoundGateCloseStart()
-	{
-		PlaySoundSet( m_SoundGate_Start, SOUND_SAFE_CLOSE_START, 0.1, 0.1 );
-	}
+    override protected void SoundGateCloseStart()
+    {
+        PlaySoundSet( m_SoundGate_Start, SOUND_SAFE_CLOSE_START, 0.1, 0.1 );
+    }
 
-	override protected void SoundGateCloseEnd()
-	{
-		PlaySoundSet( m_SoundGate_End, SOUND_SAFE_CLOSE_END, 0.1, 0.1 );
-	}
+    override protected void SoundGateCloseEnd()
+    {
+        PlaySoundSet( m_SoundGate_End, SOUND_SAFE_CLOSE_END, 0.1, 0.1 );
+    }
 
-	override bool AllowDismantle()
-	{
-		return false;
-	}
+    override bool AllowDismantle()
+    {
+        return false;
+    }
 
-	override void SetActions()
-	{
-		super.SetActions();
-		
-		RemoveAction(ActionOpenMoreDoor);
-		AddAction(ActionOpenFence);
-	}
+    override void SetActions()
+    {
+        super.SetActions();
 
-	
-	
+        RemoveAction(ActionOpenMoreDoor);
+        AddAction(ActionOpenFence);
+    }
+
+
+
 };
 
 class MoreDoorSmall extends MoreDoorBase
 {
-	override string j_moreDoorKit()
-	{
-		return "MoreDoorSmallKit";
-	}
+    override string j_moreDoorKit()
+    {
+        return "MoreDoorSmallKit";
+    }
 };
 
 class MoreDoorSmallShanty extends MoreDoorBase
 {
-	override string j_moreDoorKit()
-	{
-		return "MoreDoorSmallShantyKit";
-	}
+    override string j_moreDoorKit()
+    {
+        return "MoreDoorSmallShantyKit";
+    }
 };
 
 class MoreDoorSmallMetal extends MoreDoorBase
 {
-	override string j_moreDoorKit()
-	{
-		return "MoreDoorSmallMetalKit";
-	}
+    override string j_moreDoorKit()
+    {
+        return "MoreDoorSmallMetalKit";
+    }
 
-	override bool IsMetalDoor()
-	{
-		return true;
-	}
+    override bool IsMetalDoor()
+    {
+        return true;
+    }
 };
 
 class MoreDoorBarricade extends MoreDoorBase
 {
-	override string j_moreDoorKit()
-	{
-		return "MoreDoorBarricadeKit";
-	}
+    override string j_moreDoorKit()
+    {
+        return "MoreDoorBarricadeKit";
+    }
 
-	override void SetActions()
-	{
-	
-	}
-
-	bool hasTheGoodStuff()
-	{
-		int metalCost = 3;
-
-		int slot_id = InventorySlots.GetSlotIdFromString("Material_MetalSheets");
-		ItemBase slotCast = ItemBase.Cast( GetInventory().FindAttachment(slot_id) );
-
-		if( slotCast != NULL )
-		{
-			int j_Count = slotCast.GetQuantity();
-			if( j_Count >= metalCost )
-			{
-				return true;
-			}
-			return false;
-		}
-		return false;
-	}
-
-	// override bool CanReceiveAttachment( EntityAI attachment, int slotId )
-	// {
-	// 	int slot_id = InventorySlots.GetSlotIdFromString("Material_MetalSheets");
-	// 	ItemBase slotCast = ItemBase.Cast( GetInventory().FindAttachment(slot_id) );
-	// 	ItemBase m_attachment = ItemBase.Cast(attachment);
-
-	// 	int j_Count = slotCast.GetQuantity();
-	// 	int attachmentQuantity = m_attachment.GetQuantity();
-	// 	int remainingSlots = 3 - attachmentQuantity;
-
-	// 	if ( j_Count >= 3 || attachmentQuantity >= remainingSlots )
-	// 	{
-	// 		return false;
-	// 	}
-		
-	// 	return true;
-	// }
-
-	override void UpdateAttachmentPhysics( string slot_name, bool is_locked )
-	{
-
-	}
-
-	override void UpdateAttachmentVisuals( string slot_name, bool is_locked )
-	{
-		
-	}
-
-
-	
-};
-
-class MoreDoorMetalBarricade extends MoreDoorBase
-{
-	override string j_moreDoorKit()
-	{
-		return "MoreDoorBarricadeKit";
-	}
-
-	override void EEItemDetached ( EntityAI item, string slot_name )
+    override void SetActions()
     {
 
     }
 
-	override void SetActions()
-	{
-	
-	}
+    bool hasTheGoodStuff()
+    {
+        int metalCost = 3;
+
+        int slot_id = InventorySlots.GetSlotIdFromString("Material_MetalSheets");
+        ItemBase slotCast = ItemBase.Cast( GetInventory().FindAttachment(slot_id) );
+
+        if( slotCast != NULL )
+        {
+            int j_Count = slotCast.GetQuantity();
+            if( j_Count >= metalCost )
+            {
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
+    // override bool CanReceiveAttachment( EntityAI attachment, int slotId )
+    // {
+    //  int slot_id = InventorySlots.GetSlotIdFromString("Material_MetalSheets");
+    //  ItemBase slotCast = ItemBase.Cast( GetInventory().FindAttachment(slot_id) );
+    //  ItemBase m_attachment = ItemBase.Cast(attachment);
+
+    //  int j_Count = slotCast.GetQuantity();
+    //  int attachmentQuantity = m_attachment.GetQuantity();
+    //  int remainingSlots = 3 - attachmentQuantity;
+
+    //  if ( j_Count >= 3 || attachmentQuantity >= remainingSlots )
+    //  {
+    //      return false;
+    //  }
+
+    //  return true;
+    // }
+
+    override void UpdateAttachmentPhysics( string slot_name, bool is_locked )
+    {
+
+    }
+
+    override void UpdateAttachmentVisuals( string slot_name, bool is_locked )
+    {
+
+    }
+
+
+
+};
+
+class MoreDoorMetalBarricade extends MoreDoorBase
+{
+    override string j_moreDoorKit()
+    {
+        return "MoreDoorBarricadeKit";
+    }
+
+    override void EEItemDetached ( EntityAI item, string slot_name )
+    {
+
+    }
+
+    override void SetActions()
+    {
+
+    }
 };
 
 class MoreDoorSafe extends MoreDoorBase
 {
-	const string SOUND_SAFE_OPEN_START			= "MoreDoor_doorMetalSmallOpen_SoundSet";
-	const string SOUND_SAFE_CLOSE_START		= "MoreDoor_doorMetalSmallClose_SoundSet";
-	const string SOUND_SAFE_CLOSE_END 		= "combinationlock_close_SoundSet";
+    const string SOUND_SAFE_OPEN_START          = "MoreDoor_doorMetalSmallOpen_SoundSet";
+    const string SOUND_SAFE_CLOSE_START     = "MoreDoor_doorMetalSmallClose_SoundSet";
+    const string SOUND_SAFE_CLOSE_END       = "combinationlock_close_SoundSet";
 
-	override bool CanPutInCargo( EntityAI parent )
-	{	
-		if ( GetNumberOfItems() == 0 && !IsOpened() && !IsLocked() )
-		{
-			return true;
-		}
-		return false;
-	}
+    override bool CanPutInCargo( EntityAI parent )
+    {
+        if ( GetNumberOfItems() == 0 && !IsOpened() && !IsLocked() )
+        {
+            return true;
+        }
+        return false;
+    }
 
-	override bool IsTwoHandedBehaviour()
-	{
-		return true;
-	}
-	
-	override bool CanPutIntoHands(EntityAI parent)
-	{
-		if ( GetNumberOfItems() == 0 && !IsOpened() && !IsLocked() )
-		{
-			return true;
-		}
-		return false;
-	}
+    override bool IsTwoHandedBehaviour()
+    {
+        return true;
+    }
 
-	override bool IsPlayerInside( PlayerBase player, string selection )
-	{
-		return true;
-	}
+    override bool CanPutIntoHands(EntityAI parent)
+    {
+        if ( GetNumberOfItems() == 0 && !IsOpened() && !IsLocked() )
+        {
+            return true;
+        }
+        return false;
+    }
 
-	override bool CanRemoveFromCargo( EntityAI parent )
-	{
-		return true;
-	}
+    override bool IsPlayerInside( PlayerBase player, string selection )
+    {
+        return true;
+    }
 
-	override void OpenFence()
-	{
-		super.OpenFence();
-		GetInventory().UnlockInventory(HIDE_INV_FROM_SCRIPT);
-	}
+    override bool CanRemoveFromCargo( EntityAI parent )
+    {
+        return true;
+    }
 
-	override void CloseFence()
-	{
-		super.CloseFence();
-		GetInventory().LockInventory(HIDE_INV_FROM_SCRIPT);
+    override void OpenFence()
+    {
+        super.OpenFence();
+        GetInventory().UnlockInventory(HIDE_INV_FROM_SCRIPT);
+    }
 
-	}
+    override void CloseFence()
+    {
+        super.CloseFence();
+        GetInventory().LockInventory(HIDE_INV_FROM_SCRIPT);
 
-	override void AfterStoreLoad()
-	{	
-		SetGateState( true );
-		SetBaseState( true );
-		GetInventory().LockInventory(HIDE_INV_FROM_SCRIPT);
-	}
+    }
 
-	override void EEItemAttached ( EntityAI item, string slot_name )
-	{
-		super.EEItemAttached ( item, slot_name );
+    override void AfterStoreLoad()
+    {
+        SetGateState( true );
+        SetBaseState( true );
+        GetInventory().LockInventory(HIDE_INV_FROM_SCRIPT);
+    }
 
-		if ( slot_name == "Att_CombinationLock" )
-		{
-			CloseFence();
-		}
-	}
+    override void EEItemAttached ( EntityAI item, string slot_name )
+    {
+        super.EEItemAttached ( item, slot_name );
 
-	override void EEItemDetached ( EntityAI item, string slot_name )
-	{
-		super.EEItemDetached ( item, slot_name );
+        if ( slot_name == "Att_CombinationLock" )
+        {
+            CloseFence();
+        }
+    }
 
-		if ( slot_name == "Att_CombinationLock" )
-		{
-			OpenFence();
-		}
+    override void EEItemDetached ( EntityAI item, string slot_name )
+    {
+        super.EEItemDetached ( item, slot_name );
 
-	}
+        if ( slot_name == "Att_CombinationLock" )
+        {
+            OpenFence();
+        }
 
-	override bool AllowDestruction()
-	{
-		return false;
-	}
+    }
 
-	override void EEInit()
-	{
-		super.EEInit();
-		
-		GetInventory().LockInventory(HIDE_INV_FROM_SCRIPT);
-	}
+    override bool AllowDestruction()
+    {
+        return false;
+    }
 
-	override bool AllowDismantle()
-	{
-		return false;
-	}
+    override void EEInit()
+    {
+        super.EEInit();
 
-	override protected void SoundGateOpenStart()
-	{
-		PlaySoundSet( m_SoundGate_Start, SOUND_SAFE_OPEN_START, 0.1, 0.1 );
-	}
+        GetInventory().LockInventory(HIDE_INV_FROM_SCRIPT);
+    }
 
-	override protected void SoundGateCloseStart()
-	{
-		PlaySoundSet( m_SoundGate_Start, SOUND_SAFE_CLOSE_START, 0.1, 0.1 );
-	}
+    override bool AllowDismantle()
+    {
+        return false;
+    }
 
-	override protected void SoundGateCloseEnd()
-	{
-		PlaySoundSet( m_SoundGate_End, SOUND_SAFE_CLOSE_END, 0.1, 0.1 );
-	}
+    override protected void SoundGateOpenStart()
+    {
+        PlaySoundSet( m_SoundGate_Start, SOUND_SAFE_OPEN_START, 0.1, 0.1 );
+    }
+
+    override protected void SoundGateCloseStart()
+    {
+        PlaySoundSet( m_SoundGate_Start, SOUND_SAFE_CLOSE_START, 0.1, 0.1 );
+    }
+
+    override protected void SoundGateCloseEnd()
+    {
+        PlaySoundSet( m_SoundGate_End, SOUND_SAFE_CLOSE_END, 0.1, 0.1 );
+    }
 
 };
