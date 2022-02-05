98f86a5c9e1ca2686876efd43586bdabeb5a3f87
Tim Jensen <tim.l.jensen@gmail.com>
2022-02-04 18:53:56 -0600

    Tabs are gross

--- MoreDoors/scripts/4_world/actions/actiondialcombinationlockonmoredoor.c
+++ MoreDoors/scripts/4_world/actions/actiondialcombinationlockonmoredoor.c
@@ -1,80 +1,79 @@
 class ActionDialCombinationLockOnMoreDoor: ActionContinuousBase
 {
-	void ActionDialCombinationLockOnMoreDoor()
-	{
-		m_CallbackClass = ActionDialCombinationLockOnTargetCB;
-		m_CommandUID = DayZPlayerConstants.CMD_ACTIONMOD_OPENITEM;
-		m_CommandUIDProne = DayZPlayerConstants.CMD_ACTIONMOD_OPENITEM;		
-		m_SpecialtyWeight = UASoftSkillsWeight.ROUGH_LOW;
-	}
-	
-	override void CreateConditionComponents()  
-	{	
-		m_ConditionItem = new CCINotPresent;
-		m_ConditionTarget = new CCTNonRuined( UAMaxDistances.DEFAULT );
-	}
-	
-	override bool HasProneException()
-	{
-		return true;
-	}
+    void ActionDialCombinationLockOnMoreDoor()
+    {
+        m_CallbackClass = ActionDialCombinationLockOnTargetCB;
+        m_CommandUID = DayZPlayerConstants.CMD_ACTIONMOD_OPENITEM;
+        m_CommandUIDProne = DayZPlayerConstants.CMD_ACTIONMOD_OPENITEM;
+        m_SpecialtyWeight = UASoftSkillsWeight.ROUGH_LOW;
+    }
 
-	override string GetText()
-	{
-		PlayerBase player = PlayerBase.Cast( GetGame().GetPlayer() );
-		string combination_lock_text;
-		
-		if ( player )
-		{
-			ConstructionActionData construction_action_data = player.GetConstructionActionData();
-			combination_lock_text = construction_action_data.GetDialNumberText();
-		}		
+    override void CreateConditionComponents()
+    {
+        m_ConditionItem = new CCINotPresent;
+        m_ConditionTarget = new CCTNonRuined( UAMaxDistances.DEFAULT );
+    }
 
-		return "#dial_combination_lock" + " " + combination_lock_text;	
-	}
+    override bool HasProneException()
+    {
+        return true;
+    }
 
-	override typename GetInputType()
-	{
-		return ContinuousInteractActionInput;
-	}
+    override string GetText()
+    {
+        PlayerBase player = PlayerBase.Cast( GetGame().GetPlayer() );
+        string combination_lock_text;
 
-	override bool ActionCondition ( PlayerBase player, ActionTarget target, ItemBase item )
-	{	
-		Object targetObject = target.GetObject();
-		if ( targetObject && targetObject.CanUseConstruction() )
-		{
-			MoreDoorBase fence = MoreDoorBase.Cast( targetObject );
-			
-			if ( fence && fence.IsLocked() )
-			{
+        if ( player )
+        {
+            ConstructionActionData construction_action_data = player.GetConstructionActionData();
+            combination_lock_text = construction_action_data.GetDialNumberText();
+        }
 
-				
-					ConstructionActionData construction_action_data = player.GetConstructionActionData();
-					construction_action_data.SetCombinationLock( fence.GetCombinationLock() );
-					
-					return true;
-				
-			}
-		}
-		
-		return false;
-	}
+        return "#dial_combination_lock" + " " + combination_lock_text;
+    }
 
-	override void OnFinishProgressServer( ActionData action_data )
-	{	
-		//set dialed number
-		ConstructionActionData construction_action_data = action_data.m_Player.GetConstructionActionData();
-		CombinationLock combination_lock =  construction_action_data.GetCombinationLock();
-		if ( combination_lock )
-		{
-			combination_lock.DialNextNumber();		
-	
-			//check for unlock state
-			if ( !combination_lock.IsLockedOnGate() )
-			{
-				EntityAI target_entity = EntityAI.Cast( action_data.m_Target.GetObject() );
-				combination_lock.UnlockServer( action_data.m_Player, target_entity );
-			}
-		}
-	}
+    override typename GetInputType()
+    {
+        return ContinuousInteractActionInput;
+    }
+
+    override bool ActionCondition ( PlayerBase player, ActionTarget target, ItemBase item )
+    {
+        Object targetObject = target.GetObject();
+        if ( targetObject && targetObject.CanUseConstruction() )
+        {
+            MoreDoorBase fence = MoreDoorBase.Cast( targetObject );
+
+            if ( fence && fence.IsLocked() )
+            {
+
+
+                    ConstructionActionData construction_action_data = player.GetConstructionActionData();
+                    construction_action_data.SetCombinationLock( fence.GetCombinationLock() );
+
+                    return true;
+            }
+        }
+
+        return false;
+    }
+
+    override void OnFinishProgressServer( ActionData action_data )
+    {
+        //set dialed number
+        ConstructionActionData construction_action_data = action_data.m_Player.GetConstructionActionData();
+        CombinationLock combination_lock =  construction_action_data.GetCombinationLock();
+        if ( combination_lock )
+        {
+            combination_lock.DialNextNumber();
+
+            //check for unlock state
+            if ( !combination_lock.IsLockedOnGate() )
+            {
+                EntityAI target_entity = EntityAI.Cast( action_data.m_Target.GetObject() );
+                combination_lock.UnlockServer( action_data.m_Player, target_entity );
+            }
+        }
+    }
 }
