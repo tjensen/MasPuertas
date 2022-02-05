6fe755cf1a8bd61e4df3742e9bf359766edcd03b
Tim Jensen <tim.l.jensen@gmail.com>
2022-02-04 18:01:46 -0600

    Tabs are gross

--- MoreDoors/scripts/4_world/recipes/moddedrecipe.c
+++ MoreDoors/scripts/4_world/recipes/moddedrecipe.c
@@ -1,12 +1,11 @@
 modded class PluginRecipesManagerBase
 {
-	override void RegisterRecipies()
-	{
-		super.RegisterRecipies();
-		RegisterRecipe(new craft_MoreDoorSmall);
-		RegisterRecipe(new craft_MoreDoorMetalSmall);
-		RegisterRecipe(new craft_MoreDoorShanty);
-		RegisterRecipe(new craft_MoreDoorBarricade);
-
-	}	
+    override void RegisterRecipies()
+    {
+        super.RegisterRecipies();
+        RegisterRecipe(new craft_MoreDoorSmall);
+        RegisterRecipe(new craft_MoreDoorMetalSmall);
+        RegisterRecipe(new craft_MoreDoorShanty);
+        RegisterRecipe(new craft_MoreDoorBarricade);
+    }
 }
