21f075dec2eeac604746d25b8e1c28001390c569
Tim Jensen <tim.l.jensen@gmail.com>
2022-02-04 19:14:26 -0600

    Prevent desync when unlocking safe by unlocking the safe's inventory before trying to detach the lock

--- MoreDoors/scripts/4_world/actions/actiondialcombinationlockonmoredoor.c
+++ MoreDoors/scripts/4_world/actions/actiondialcombinationlockonmoredoor.c
@@ -11,7 +11,7 @@ class ActionDialCombinationLockOnMoreDoor: ActionContinuousBase
     override void CreateConditionComponents()
     {
         m_ConditionItem = new CCINotPresent;
-        m_ConditionTarget = new CCTNonRuined( UAMaxDistances.DEFAULT );
+        m_ConditionTarget = new CCTNonRuined(UAMaxDistances.DEFAULT);
     }
 
     override bool HasProneException()
@@ -21,10 +21,10 @@ class ActionDialCombinationLockOnMoreDoor: ActionContinuousBase
 
     override string GetText()
     {
-        PlayerBase player = PlayerBase.Cast( GetGame().GetPlayer() );
+        PlayerBase player = PlayerBase.Cast(GetGame().GetPlayer());
         string combination_lock_text;
 
-        if ( player )
+        if (player)
         {
             ConstructionActionData construction_action_data = player.GetConstructionActionData();
             combination_lock_text = construction_action_data.GetDialNumberText();
@@ -38,42 +38,46 @@ class ActionDialCombinationLockOnMoreDoor: ActionContinuousBase
         return ContinuousInteractActionInput;
     }
 
-    override bool ActionCondition ( PlayerBase player, ActionTarget target, ItemBase item )
+    override bool ActionCondition(PlayerBase player, ActionTarget target, ItemBase item)
     {
         Object targetObject = target.GetObject();
-        if ( targetObject && targetObject.CanUseConstruction() )
+        if (targetObject && targetObject.CanUseConstruction())
         {
-            MoreDoorBase fence = MoreDoorBase.Cast( targetObject );
+            MoreDoorBase fence = MoreDoorBase.Cast(targetObject);
 
-            if ( fence && fence.IsLocked() )
+            if (fence && fence.IsLocked())
             {
+                ConstructionActionData construction_action_data = player.GetConstructionActionData();
+                construction_action_data.SetCombinationLock(fence.GetCombinationLock());
 
-
-                    ConstructionActionData construction_action_data = player.GetConstructionActionData();
-                    construction_action_data.SetCombinationLock( fence.GetCombinationLock() );
-
-                    return true;
+                return true;
             }
         }
 
         return false;
     }
 
-    override void OnFinishProgressServer( ActionData action_data )
+    override void OnFinishProgressServer(ActionData action_data)
     {
         //set dialed number
         ConstructionActionData construction_action_data = action_data.m_Player.GetConstructionActionData();
         CombinationLock combination_lock =  construction_action_data.GetCombinationLock();
-        if ( combination_lock )
+        if (combination_lock)
         {
             combination_lock.DialNextNumber();
 
             //check for unlock state
-            if ( !combination_lock.IsLockedOnGate() )
+            if (!combination_lock.IsLockedOnGate())
             {
-                EntityAI target_entity = EntityAI.Cast( action_data.m_Target.GetObject() );
-                combination_lock.UnlockServer( action_data.m_Player, target_entity );
+                EntityAI target_entity = EntityAI.Cast(action_data.m_Target.GetObject());
+
+                // Unlock inventory before detaching lock to prevent inventory desync
+                target_entity.GetInventory().UnlockInventory(HIDE_INV_FROM_SCRIPT);
+
+                combination_lock.UnlockServer(action_data.m_Player, target_entity);
             }
         }
     }
-}
+};
+
+// vim:ft=enforce
